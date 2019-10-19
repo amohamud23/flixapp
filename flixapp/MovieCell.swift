e16e245e4e35bac776482bcecb0f32e0f6b9156c
Abdi <amohamud23@gmail.com>
2019-10-18 23:03:09 -0700

    Initial Commit

---
+++ flixapp/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  flixapp
+//
+//  Created by Abdi Mohamud on 10/18/19.
+//  Copyright © 2019 Abdi Mohamud. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
