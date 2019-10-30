7f3196300744002744c0996749e3d1f2ad711d23
Abdi <amohamud23@gmail.com>
2019-10-29 23:39:47 -0700

    implemented a grid view for superheroes

---
+++ flixapp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixapp
+//
+//  Created by Abdi Mohamud on 10/29/19.
+//  Copyright © 2019 Abdi Mohamud. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
